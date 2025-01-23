65df9eec784da5c695a1a36f8abcf776eacc8a0a
tderamus <deramust@gmail.com>
2025-01-22 19:10:48 -0600

    created Product Class with Name, Price, and ProductTypeId properties

--- BrassAndPoem/Product.cs
+++ BrassAndPoem/Product.cs
@@ -1,2 +1,8 @@
 //create your Product class here
 
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int ProductTypeId { get; set; }
+}
